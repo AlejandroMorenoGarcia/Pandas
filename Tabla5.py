e24e4a2cdc1a8b65e29e858c9fa854ff289f1395
añei <adanaila080@gmail.com>
2026-04-25 18:35:53 +0200

    Tabla 5 añadida

---
+++ Tabla5.py
@@ -0,0 +1,38 @@
+import plotly.express as px
+from Tablas import Tablas
+
+
+class Tabla2(Tablas):
+    def __init__(self, df):
+        
+        super().__init__(df, "¿Dormir más mejora el rendimiento?")
+
+    def getTabla(self):
+        
+        color_discrete_map = {
+            "Male": "#2979FF",  
+            "Female": "#FF1744" 
+        }
+
+       
+        fig = px.violin(
+            self.df,
+            box=True,
+            points="all",
+            x='Sleep_Hours',
+            y='Exam_Score',
+            color_discrete_map=color_discrete_map,
+            title=self.titulo,
+            labels={
+                'Sleep_Hours': 'Horas de Sueño',
+                'Exam_Score': 'Puntuación del Examen',
+            },
+            template='plotly_dark'
+        )
+        
+        
+        fig.show()
+        
+        
+
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from DataFrame import DataFrame
 from Menu import Menu
 from Tabla1 import Tabla1
-
+from Tabla2 import Tabla2
 
 
 def main():
@@ -9,6 +9,7 @@ def main():
     df = df_object.getdf()
     menu = Menu()
     menu.add_option(Tabla1(df))
+    menu.add_option(Tabla2(df))
     menu.print_options()
     opcion = int(input("Escoje una opcion: "))
     menu.get_option(opcion)
