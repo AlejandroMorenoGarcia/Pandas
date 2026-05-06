8d792cdb098b80458f9ab52e42f69f35b7d408eb
alemorgar7@alu.edu.gva.es <ciclost@localhost>
2026-05-06 19:49:08 +0200

    Añadido los comentarios a tablas 1 y 2

--- Tabla1.py
+++ Tabla1.py
@@ -1,42 +1,57 @@
 import plotly.express as px
 from Tablas import Tablas
 
+# Grafico sobre la influencia de la asistencia y las horas de estudio en las notas
 class Tabla1(Tablas):
+    # Constructor de la clase
     def __init__(self,df):
         super().__init__(df, "¿Influye la asistencia y las horas de estudio en las notas?")
 
+    # Funcion de la creacion de la tabla
     def getTabla(self):
+        """
+            En esta grafica vamos a comprobar la importancia de atender a clase y
+            las horas que estudiamos a la hora de la mejora de nuestras notas en la
+            que modificaremos nuestro Data Frame principal para poder hacer un 
+            count de la las agrupaciones que coincidan para hacer los puntos de 
+            la tabla mas grandes segun este count
+        """
+
+        # Variable para los colores de los posibles valores
         color_discrete_map = {
             "Male": "#2979FF",  # azul eléctrico
             "Female": "#FF1744"  # rojo/rosa intenso
         }
 
+        # Modificamos el Data Frame principal para quedarnos con las columnas que queremos y añadir el count
         df_largo = self.df.groupby(["Hours_Studied", "Exam_Score", "Gender", "Attendance"]).size().reset_index(name="Count")
 
+        # Creacion de la grafica
         fig = px.scatter_3d(
-            df_largo,
-            x="Hours_Studied",
-            y="Attendance",
-            z="Exam_Score",
-            title=self.titulo,
-            color="Gender",
-            color_discrete_map=color_discrete_map,
-            size="Count",
-            labels={
+            df_largo, # Data Frame modificado
+            x="Hours_Studied", # Eje X: Horas de estudio
+            y="Attendance", # Eje Y: Atencion en clase
+            z="Exam_Score", # Eje Z: Notas de los examenes
+            title=self.titulo, # Titulo de la grafica
+            color="Gender", # Color diferenciado por genero
+            color_discrete_map=color_discrete_map, # Opciones de los colores
+            size="Count", # Radio segun cantidad
+            labels={ # Renombre de las distintas opciones
                 "Hours_Studied": "Horas de Estudio",
                 "Exam_Score": "Puntuación del Examen",
                 "Attendance": "Asistencia a Clase",
             },
-            template="plotly_dark",
-            size_max=30
+            template="plotly_dark", # Estilo de fondo de la Grafica
         )
 
+        # Adaptamos la grafica para que tenga forma de cubo
         fig.update_layout(
             scene=dict(
                 aspectmode="cube",
             )
         )
 
+        # Quitamos el borde de los de los valores
         fig.update_traces(marker=dict(
             line=dict(
                 width=0,
@@ -44,4 +59,4 @@ class Tabla1(Tablas):
             )
         ))
 
-        fig.show()
+        fig.show() # Mostramos la grafica
--- Tabla2.py
+++ Tabla2.py
@@ -1,50 +1,65 @@
 import plotly.express as px
 from Tablas import Tablas
 
+# Grafivo sobre la importancia de los profesores en la mejora de las notas
 class Tabla2(Tablas):
+    # Constructor de la clase
     def __init__(self,df):
         super().__init__(df, "¿Cómo afecta la calidad del profesor a la mejora de las notas?")
 
+    # Funcion de la creacion de la tabla
     def getTabla(self):
+        """
+            Esta grafica utilizaremos utilizaremos el Data Frame principal sin modificaciones 
+            ya que no sera necesario para la obtencion de los datos que ya tenemos
+        """
+
+        # Variable para los colores de los posibles valores
         color_discrete_map = {
             "Low": "#FF6B6B",  # rojo coral
             "Medium": "#4ECDC4",  # turquesa
             "High": "#FFE66D",  # amarillo dorado
         }
 
+        # Creacion de la grafica
         fig = px.scatter(
-            self.df,
-            x="Previous_Scores",
-            y="Exam_Score",
-            color="Teacher_Quality",  # color por calidad del profesor
-            trendline="lowess",
-            trendline_options = {"frac": 0.2},
-            title=self.titulo,
-            labels={
+            self.df, # Data Frame principar
+            x="Previous_Scores", # Eje X: Anteriores notas
+            y="Exam_Score", # Eje Y: Notas actuales
+            color="Teacher_Quality",  # Color por calidad del profesor
+            trendline="lowess", # Linea de tendencia de las graficas para cada calidad del profesor 
+            trendline_options = {"frac": 0.2}, # 
+            title=self.titulo, # Titulo de la grafica
+            labels={ # Renombre de las distintas opciones
                 "Previous_Scores": "Puntuaciones Anteriores",
                 "Exam_Score": "Puntuación del Examen",
                 "Teacher_Quality": "Calidad del Profesor",
             },
-            opacity=0.6,
-            template="plotly_dark",
-            color_discrete_map= color_discrete_map
+            opacity=0.6, # Opacidad de los valores
+            template="plotly_dark", # Estilo de fondo de la Grafica
+            color_discrete_map= color_discrete_map # Opciones de los colores
         )
+        """
+            Vamos a generar una linea extra que pase por la diagonal para ver los 
+            alumnos que han mejorado su nota y los que la han empeorado
+        """
 
-        min_val = self.df["Exam_Score"].min()
-        max_val = 100
+        min_val = self.df["Exam_Score"].min() # Variable para el valor minimo de la diagonal
+        max_val = 100 # Variable para el valor maximo de la diagonal
 
-        fig.add_shape(
-            type="line",
-            x0=min_val, y0=min_val,
-            x1=max_val, y1=max_val,
-            line=dict(
-                color="white",
-                width=2,
-                dash="dash"
+        # Creacion de la linea 
+        fig.add_shape( # Añadimos a la figura la linea
+            type="line", # Tipo Linea
+            x0=min_val, y0=min_val, # Valores minimos de la linea en los ejes 
+            x1=max_val, y1=max_val, # Valores maximos de la linea en los ejes 
+            line=dict( # Opciones de la linea
+                color="white", # Color Blanco
+                width=2, # Ancho
+                dash="dash" # Tipo punteado
             )
         )
 
-        # Añadir a la leyenda
+        # La añadimos a la leyenda
         fig.add_scatter(
             x=[None], y=[None],
             mode="lines",
@@ -52,4 +67,4 @@ class Tabla2(Tablas):
             name="Referencia y=x"
         )
 
-        fig.show()
+        fig.show() # Mostramos la grafica
