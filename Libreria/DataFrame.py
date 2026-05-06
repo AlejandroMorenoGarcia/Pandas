1644000c58527bbe61abd38c95a2fe83fc19bba2
alemorgar7@alu.edu.gva.es <ciclost@localhost>
2026-05-06 20:28:02 +0200

    Refactorizacion de toda la libreria

--- Libreria/DataFrame.py
+++ Libreria/DataFrame.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 class DataFrame:
     def __init__(self):
-        self.df = pd.read_csv("StudentPerformanceFactors_realistic.csv")
+        self.df = pd.read_csv("../CSVs/StudentPerformanceFactors_realistic.csv")
         
     def getdf(self):
         return self.df
