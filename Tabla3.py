5a8a1ee75ac00b5fb40964d0594ba1837f9d4173
ivan <ivafersau@alu.edu.gva.es>
2026-05-09 19:25:50 +0200

    Version de ivan 09/05/26

---
+++ Tabla3.py
@@ -0,0 +1,43 @@
+import plotly.express as px
+from Tablas import Tablas
+
+class Tabla3(Tablas):
+    #Constructo de la clase
+    def __init__(self, df):
+        super().__init__(df, "Relación entre nivel socioeconómico y notas (media)")
+
+    
+    #Función principal que agrupa los datos, crea el gráfico y muestra las figura
+    def getTabla(self):
+        
+        #Función que agrupa
+        df_grouped = self.df.groupby("Family_Income", as_index=False)["Exam_Score"].mean() #Función Media
+
+        fig = px.bar(
+            df_grouped,
+            x="Family_Income",
+            y="Exam_Score",
+            text="Exam_Score",
+            color="Family_Income",
+            color_discrete_map={
+                "Low": "#4C78A8",
+                "Medium": "#54A24B",
+                "High": "#F58518"
+            },
+            template="plotly_dark",
+            labels={
+                "Family_Income": "Nivel Socioeconómico",
+                "Exam_Score": "Nota Media del Examen"
+            }
+        )
+
+        fig.update_traces(texttemplate='%{text:.2f}', textposition='outside')
+
+        fig.update_layout(
+            xaxis_title="Nivel Socioeconómico",
+            yaxis_title="Nota Media del Examen",
+            title=self.titulo,
+            yaxis_range=[66, 68]
+        )
+
+        fig.show()
